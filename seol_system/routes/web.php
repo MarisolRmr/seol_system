4a10a376a278ef55aa7656cf073511ac2a0b3f2d
“MarisolRmr” <2030112@upv.edu.mx>
2023-12-04 10:32:07 -0600

    rutas

--- seol_system/routes/web.php
+++ seol_system/routes/web.php
@@ -38,10 +38,7 @@ Route::get('/estudiante/enproceso', [EstudianteController::class, 'enproceso'])-
 
 
 Route::get('/Admin/dashboard', [AdminController::class, 'admin'])->name('Admin.dashboard');
-
 Route::get('/Admin/AgregarAlumno', [AdminController::class, 'agregarAlumno'])->name('admin.alumnoagregar');
-
 Route::get('/Admin/AgregarOficinista', [AdminController::class, 'agregarOficinista'])->name('admin.alumnoOficinista');
-
 Route::get('/Admin/GestionPlantilla', [AdminController::class, 'gestionPlantillaView'])->name('admin.gestionPlantilla');
 
