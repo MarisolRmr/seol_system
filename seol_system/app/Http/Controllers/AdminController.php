eaecff3652cd89adba69e5cf8c9014a2f4f2332f
JorgeLCTorres <2030340@upv.edu.mx>
2023-12-05 05:21:29 -0600

    Prueba en AdminController

--- seol_system/app/Http/Controllers/AdminController.php
+++ seol_system/app/Http/Controllers/AdminController.php
@@ -1,83 +1,123 @@
+
 <?php
 
 namespace App\Http\Controllers;
 
-use App\Models\User;
+use App\Http\Controllers\Controller;
+use App\Models\Proveedor;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Hash;
 
-class AdminController extends Controller
+class ProveedorController extends Controller
 {
-    //
-    public function admin(){
-        return view('Admin.dashboard');
+    /**
+     * Constructor to validate authenticated user.
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+    
+
+    //regresar la vista para mostrar todos los proveedores
+    public function index()
+    {
+        $proveedores = Proveedor::all();
+        return view('pages/proveedores/proveedor', ['proveedores' => $proveedores]);
     }
 
-    public function agregarAlumno(){
-        return view('Admin.AñadirAlumno');
+    //regresar la vista para crear un nuevo proveedor
+    public function create()
+    {
+        return view('pages/proveedores/add-proveedor');
     }
 
-    public function gestionPlantillaView(){
-        return view('Admin.GestionarPlantilla');
-    }
-
-    public function agregarOficinista(){
-        return view('Admin.AgregarOficinista');
-    }
-
-    //Funcion para almacenar Alumnos en la base de datos
-    public function storeAlumno(Request $request){
-        //Se validan los campos
+    //regresar la vista para agreegar un proveedor
+    
+    //funcion para agregar un nuevo proveedor
+    public function store(Request $request)
+    {
+        //validar los datos
         $this->validate($request, [
-            'nombre' => 'required',
-            'apellidopaterno' => 'required',
-            'apellidomaterno' => 'required',
-            'email' => 'required|unique:users',
-            'password' => 'required',
-            'curp' => 'required',
-            'matricula' => 'required',
-            'carrera' => 'required',
+            'nombre_empresa' => 'required | unique:proveedores',
+            'contacto_principal' => 'required',
+            'direccion' => 'required',
+            'telefono' => 'required | numeric | digits:10 | unique:proveedores',
+            'email' => 'required | email | unique:proveedores',
+            'sitio_web' => 'required | unique:proveedores',
+            'rfc' => 'required | max:13 | min:12 | unique:proveedores',
             
         ]);
-        //Se añade el registro a la base de datos
-        User::create([
-            'nombre' => $request->nombre,
-            'apellido_paterno' => $request->apellidopaterno,
-            'apellido_materno' => $request->apellidomaterno,
-            'password' => Hash::make($request->password),
-            'matricula' => $request->matricula,
-            'carrera' => $request->carrera,
+
+        //guardar los datos
+        Proveedor::create([
+            'nombre_empresa' => $request->nombre_empresa,
+            'contacto_principal' => $request->contacto_principal,
+            'direccion' => $request->direccion,
+            'telefono' => $request->telefono,
             'email' => $request->email,
-            'curp' => $request->curp,
-            'rol' => 3,
+            'sitio_web' => $request->sitio_web,
+            'rfc' => $request->rfc,
         ]);
-        return redirect()->route('admin.alumnoagregar')->with('success', 'El usuario ha sido registrado correctamente');
+
+        //redireccionar a la vista de productos
+        return redirect()->route('proveedor.index')->with('success', 'Proveedor agregado correctamente');
+    }
+    
+    //regresar la vista para editar un proveedor
+    //obtener el id del proveedor que se quiere editar
+    public function edit($id_proveedor)
+    {
+        //buscar el proveedor con el id que se recibe
+        $proveedor = Proveedor::find($id_proveedor);
+        return view('pages/proveedores/edit-proveedor', ['proveedor' => $proveedor]);
     }
 
-    //Funcion para almacenar Alumnos en la base de datos
-    public function storeOficinista(Request $request){
-        //Se validan los campos
-        $this->validate($request, [
-            'nombre' => 'required',
-            'apellidopaterno' => 'required',
-            'apellidomaterno' => 'required',
-            'email' => 'required|unique:users',
-            'password' => 'required',
-            'curp' => 'required',
-            
-        ]);
-        //Se añade el registro a la base de datos
-        User::create([
-            'nombre' => $request->nombre,
-            'apellido_paterno' => $request->apellidopaterno,
-            'apellido_materno' => $request->apellidomaterno,
-            'password' => Hash::make($request->password),
-            'matricula' => $request->matricula,
-            'carrera' => $request->carrera,
-            'email' => $request->email,
-            'curp' => $request->curp,
-            'rol' => 2,
-        ]);
-        return redirect()->route('admin.alumnoagregar')->with('success', 'El usuario ha sido registrado correctamente');
+    //funcion para actualizar un proveedor
+    public function update(Request $request, $id_proveedor)
+    {
+    // Obtener el proveedor existente
+    $proveedor = Proveedor::find($id_proveedor);
+
+    // Validar los datos, ignorando la regla 'unique' si los datos no han cambiado
+    $this->validate($request, [
+        'nombre_empresa' => 'required|unique:proveedores,nombre_empresa,'.$proveedor->id,
+        'contacto_principal' => 'required',
+        'direccion' => 'required',
+        'telefono' => 'required|numeric|digits:10|unique:proveedores,telefono,'.$proveedor->id,
+        'email' => 'required|email|unique:proveedores,email,'.$proveedor->id,
+        'sitio_web' => 'required|unique:proveedores,sitio_web,'.$proveedor->id,
+        'rfc' => 'required| max:13|min:12|unique:proveedores,rfc,'.$proveedor->id.'',
+    ]);
+
+    // Actualizar los datos
+    $proveedor->update([
+        'nombre_empresa' => $request->nombre_empresa,
+        'contacto_principal' => $request->contacto_principal,
+        'direccion' => $request->direccion,
+        'telefono' => $request->telefono,
+        'email' => $request->email,
+        'sitio_web' => $request->sitio_web,
+        'rfc' => $request->rfc,
+    ]);
+
+        //redireccionar a la vista de productos
+        return redirect()->route('proveedor.index')->with('success', 'Proveedor actualizado correctamente');
     }
+
+
+    //eliminar un proveedor
+    public function delete($id_proveedor)
+    {
+        //eliminar el proveedor con el id que se recibe
+        Proveedor::find($id_proveedor)->delete();
+        return redirect()->route('proveedor.index')->with('success', 'Proveedor eliminado correctamente');
+    }
+
+
+
+
+    
+
+
+
 }
--- seol_system/routes/web.php
+++ seol_system/routes/web.php
@@ -41,5 +41,6 @@ Route::get('/Admin/dashboard', [AdminController::class, 'admin'])->name('Admin.d
 Route::get('/Admin/AgregarAlumno', [AdminController::class, 'agregarAlumno'])->name('admin.alumnoagregar');
 Route::post('/Admin/Alumnos/create', [AdminController::class,'storeAlumno'])->name('admin.alumno.store');
 Route::get('/Admin/AgregarOficinista', [AdminController::class, 'agregarOficinista'])->name('admin.alumnoOficinista');
+Route::post('/Admin/Oficinistas/create', [AdminController::class, 'storeOficinista'])->name('admin.oficinista.store');
 Route::get('/Admin/GestionPlantilla', [AdminController::class, 'gestionPlantillaView'])->name('admin.gestionPlantilla');
 
